ede0902214ecf3d789c4dfe1e6bfa533f1059a5b
kennyan11b <kennyan11b@gmail.com>
2023-03-05 14:55:35 -0800

    Push test to ensure matching file

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -35,27 +35,27 @@ public class FilmController {
 //		model.addAttribute("TESTFILM", TEST);	
 		return "home";
 	}
-	
-	//this one just loads page - has no parameters
+
+	// this one just loads page - has no parameters
 	@GetMapping("findFilmById.do")
 	public String loadIDSearch(Model model) throws SQLException {
 		return "findFilmById";
 	}
-	
-	//this one just loads page - has no parameters
+
+	// this one just loads page - has no parameters
 	@GetMapping("addFilm.do")
 	public String addFilm(Model model) throws SQLException {
 		return "addFilm";
 	}
-	
-	//this one just loads page - has no parameters
-		@GetMapping("findFilmByKeyword.do")
-		public String loadKeywordSearch(Model model) throws SQLException {
-			return "findFilmByKeyword";
-		}
 
-	//this method is run if searching for a film by ID
-	@RequestMapping(path = "findFilmById.do", params = "id",method = RequestMethod.GET)
+	// this one just loads page - has no parameters
+	@GetMapping("findFilmByKeyword.do")
+	public String loadKeywordSearch(Model model) throws SQLException {
+		return "findFilmByKeyword";
+	}
+
+	// this method is run if searching for a film by ID
+	@RequestMapping(path = "findFilmById.do", params = "id", method = RequestMethod.GET)
 	public ModelAndView findFilmById(@RequestParam("id") int id) throws SQLException {
 		ModelAndView mv = new ModelAndView();
 		Film film = filmDAO.findFilmById(id);
@@ -63,43 +63,40 @@ public class FilmController {
 		mv.setViewName("findFilmById");
 		return mv;
 	}
-	
-	//this method is run if searching for a film by keyword
-		@RequestMapping(path = "findFilmByKeyword.do", params = "keyword",method = RequestMethod.GET)
-		public ModelAndView findFilmById(@RequestParam("keyword") String keyword) throws SQLException {
-			ModelAndView mv = new ModelAndView();
-			List<Film> films = filmDAO.findFilmByKeyWord(keyword);
-			mv.addObject("films", films);
-			mv.setViewName("findFilmByKeyword");
-			return mv;
-		}
-	
-		//adds a film
-		@RequestMapping(path="addFilm.do",method=RequestMethod.POST)
-		public ModelAndView newFilm(
-				@RequestParam("film_title") String title, 
-				@RequestParam("film_desc") String desc,
-				@RequestParam("film_year") int year,
-				@RequestParam("film_length") int length) throws SQLException {
-			//doing this the tedious way first
-			Film film = new Film();
-			film.setTitle(title);
-			film.setDescription(desc);
-			film.setFeatures(null);
-			film.setLanguageId(0);
-			film.setRating(null);
-			film.setReleaseYear(0);
-			film.setLength(null);
-			film.setRentalDuration(0);
-			film.setRentalRate(0);
-			film.setReplacementCost(0);
-			//add film to database
-			filmDAO.createFilm(film);
-			ModelAndView mv = new ModelAndView();
-			mv.setViewName("addFilm");
-			
-			return mv;
-		}
+
+	// this method is run if searching for a film by keyword
+	@RequestMapping(path = "findFilmByKeyword.do", params = "keyword", method = RequestMethod.GET)
+	public ModelAndView findFilmById(@RequestParam("keyword") String keyword) throws SQLException {
+		ModelAndView mv = new ModelAndView();
+		List<Film> films = filmDAO.findFilmByKeyWord(keyword);
+		mv.addObject("films", films);
+		mv.setViewName("findFilmByKeyword");
+		return mv;
+	}
+
+	// adds a film
+	@RequestMapping(path = "addFilm.do", method = RequestMethod.POST)
+	public ModelAndView newFilm(@RequestParam("film_title") String title, @RequestParam("film_desc") String desc,
+			@RequestParam("film_year") int year, @RequestParam("film_length") int length) throws SQLException {
+		// doing this the tedious way first
+		Film film = new Film();
+		film.setTitle(title);
+		film.setDescription(desc);
+		film.setFeatures(null);
+		film.setLanguageId(0);
+		film.setRating(null);
+		film.setReleaseYear(0);
+		film.setLength(null);
+		film.setRentalDuration(0);
+		film.setRentalRate(0);
+		film.setReplacementCost(0);
+		// add film to database
+		filmDAO.createFilm(film);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("addFilm");
+
+		return mv;
+	}
 
 //	Added this - Kenny	
 	@GetMapping("findActorById.do")
@@ -118,27 +115,27 @@ public class FilmController {
 	}
 
 //	Added this - Kenny		
-	  @GetMapping("updateFilm.do")
-	    public ModelAndView updateFilm(@RequestParam("filmId") Integer filmId) throws SQLException {
-	        ModelAndView mv = new ModelAndView();
-	        Film film = filmDAO.findFilmById(filmId);
-	        mv.addObject("film", film);
-	        mv.setViewName("updateFilm");
-	        return mv;
-	    }
+	@GetMapping("updateFilm.do")
+	public ModelAndView updateFilm(@RequestParam("filmId") Integer filmId) throws SQLException {
+		ModelAndView mv = new ModelAndView();
+		Film film = filmDAO.findFilmById(filmId);
+		mv.addObject("film", film);
+		mv.setViewName("updateFilm");
+		return mv;
+	}
 
-	    @PostMapping("updateFilm.do")
-	    public ModelAndView updateFilm(@ModelAttribute("film") Film film) {
-	        ModelAndView mv = new ModelAndView();
-	        boolean success = filmDAO.updateFilm(film.getId(), film);
-	        if (success) {
-	            mv.setViewName("updateFilmSuccess");
-	        } else {
-	            mv.addObject("errorMessage", "Unable to update film.");
-	            mv.setViewName("updateFilm");
-	        }
-	        return mv;
-	    }
+	@PostMapping("updateFilm.do")
+	public ModelAndView updateFilm(@ModelAttribute("film") Film film) {
+		ModelAndView mv = new ModelAndView();
+		boolean success = filmDAO.updateFilm(film.getId(), film);
+		if (success) {
+			mv.setViewName("updateFilmSuccess");
+		} else {
+			mv.addObject("errorMessage", "Unable to update film.");
+			mv.setViewName("updateFilm");
+		}
+		return mv;
+	}
 
 //	Added this - Kenny	
 	@GetMapping("deleteFilm.do")
